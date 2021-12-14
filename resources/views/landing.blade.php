f639ec472358641f3ab4fea0551ee4092bd24872
SolaRita <ritasolacanals@gmail.com>
2021-12-14 12:19:47 +0100

    boostrap css

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -13,7 +13,7 @@
     
 
     <!-- Bootstrap core CSS -->
-<link href="./Album example · Bootstrap v5.1_files/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link href="./boostrap/bootstrap.min.css" rel="stylesheet">
 
     <!-- Favicons -->
 <link rel="apple-touch-icon" href="https://getbootstrap.com/docs/5.1/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
