2662e66fc53c33acf8cf82d1e3e5c9c542ad7eaa
SolaRita <ritasolacanals@gmail.com>
2021-12-15 11:33:15 +0100

    for cards

--- resources/views/components/eventcard.blade.php
+++ resources/views/components/eventcard.blade.php
@@ -1,3 +1,4 @@
+@for($i=0; $i<6; $i++)
 <div class="col">
     <div class="card shadow-sm">
       <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
@@ -13,6 +14,6 @@
       </div>
     </div>
 </div>
-    
+@endfor    
 
 
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -9,9 +9,7 @@
       
 
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-      
         @include('components.eventcard')
-
       </div>
     </div>
   </div>
