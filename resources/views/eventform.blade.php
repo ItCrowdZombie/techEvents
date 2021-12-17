98261d5293959508b5193c7af855581c7c82ded6
MeliMano <melimanoni@gmail.com>
2021-12-17 09:18:24 +0100

    form added

--- resources/views/eventform.blade.php
+++ resources/views/eventform.blade.php
@@ -6,9 +6,41 @@
 
   <div class="album py-5 bg-light">
     <div class="container">
-      FORM
-
       
+      <form class="row g-3">
+        <div class="col-md-6">
+          <label for="inputTitle" class="form-label">Title</label>
+          <input type="text" class="form-control" id="inputTitle">
+        </div>
+        <div class="col-md-6">
+          <label for="inputPlazas" class="form-label">plazas totales </label>
+          <input type="number" class="form-control" id="inputPlazas">
+        </div>
+        <div class="col-12">
+          <label for="inputDescription" class="form-label">Description</label>
+          <input type="textarea" class="form-control" id="inputDescription">
+        </div>
+        <div class="col-12">
+          <label for="inputImg" class="form-label">Imagen</label>
+          <input type="url" class="form-control" id="inputImg">
+        </div>
+        <div class="col-md-6">
+          <label for="inputDate" class="form-label">Fecha del evento</label>
+          <input type="datetime" class="form-control" id="inputDate">
+        </div>
+        <div class="col-12">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="gridCheck">
+            <label class="form-check-label" for="gridCheck">
+              Destacado
+            </label>
+          </div>
+        </div>
+        <div class="col-12">
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+      </form>
+
     </div>
   </div>
         
