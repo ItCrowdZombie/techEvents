fc9334b0f5424f2a262243d2c998f1000e253f75
MayuRuru <aruizrull@protonmail.com>
2021-12-17 10:34:11 +0100

    redirect after register

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Levanos a la landingg!') }}
                 </div>
             </div>
         </div>
