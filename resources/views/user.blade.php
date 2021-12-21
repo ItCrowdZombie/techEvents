555d9487544fad750a0ab687b8f276bb0356010c
SolaRita <ritasolacanals@gmail.com>
2021-12-21 09:52:57 +0100

    user blade

---
+++ resources/views/user.blade.php
@@ -0,0 +1,19 @@
+
+@extends('layouts.app')
+
+@section( 'content')
+<main>
+
+  <div class="album py-5 bg-light">
+    <div class="container">
+      
+
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        @include('components.eventcard')
+      </div>
+      
+    </div>
+  </div>
+        
+</main>
+@endsection
