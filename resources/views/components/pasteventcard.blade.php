34b0c232269f542197f0ad40a032e4091e2a351a
SolaRita <ritasolacanals@gmail.com>
2021-12-23 12:25:32 +0100

    past cards grey

--- resources/views/components/pasteventcard.blade.php
+++ resources/views/components/pasteventcard.blade.php
@@ -16,7 +16,7 @@ $date = Carbon::now($tz);
     
 
 <div class="col">
-    <div class="card shadow-sm">
+    <div class="card shadow-sm" style="color: rgb(168, 167, 167)">
 {{--     <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
  --}}
       <div class="card-body">
@@ -24,7 +24,7 @@ $date = Carbon::now($tz);
         <img src="{{$event->img}}"alt="" class="card-img-top">
         <p class="card-text">{{$event->description}}</p>
         <p class="card-text">{{$event->event_date}}</p>
-        <p class="card-text">{{$event->max_users}}</p>
+        <p class="card-text">Max. users: {{$event->max_users}}</p>
         <div class="d-flex justify-content-between align-items-center">
 
         </div>
