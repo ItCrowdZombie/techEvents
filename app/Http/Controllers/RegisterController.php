8748b0ebbae85c1dfdca6af54caa1e0631fe5db1
LucasManuelRodriguez <rodriguez.m.lucas@gmail.com>
2022-01-11 10:36:35 -0300

    confirm  joinEvent to mail

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,13 +6,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Mail\joinEvent;
 use Illuminate\Support\Facades\Mail;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
     public function join($eventId){
         
     $user = Auth::user();
-    if (!$user->isJoined($eventId)) {$user->joinEvent()->attach($eventId); $correo = new joinEvent; Mail::to('rodriguez.m.lucas@gmail.com')->send($correo); return back();} 
+    if (!$user->isJoined($eventId)) {$user->joinEvent()->attach($eventId); $correo = new joinEvent; Mail::to($user->email)->send($correo); return back();} 
     if ($user->isJoined($eventId)) {$user->joinEvent()->detach($eventId);}
     return back();    
     }
