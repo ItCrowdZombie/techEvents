c6d8345a83f86dbce82a47d5ce11f0cec145f549
MayuRuru <aruizrull@protonmail.com>
2022-01-14 09:01:42 +0100

    center carrousel

--- resources/views/components/slider.blade.php
+++ resources/views/components/slider.blade.php
@@ -9,14 +9,18 @@
         @if(!empty ($eventHigh))    
             {{-- @if($event->is_it_featured) --}}
                 <div class="carousel-item active">
-                    <img class="d-block w-100"  src="{{$eventHigh[0]->img}}" height="300vw"  alt="{{$eventHigh[0]->title}}">
+                    <div class="d-flex justify-content-center">
+                    <img class="d-block"  src="{{$eventHigh[0]->img}}" height="300vw"  alt="{{$eventHigh[0]->title}}">
+                    </div>
                 </div>
         @endif 
 
         @for($i=1; $i < count($eventHigh);$i++)    
             {{-- @if($event->is_it_featured) --}}
                 <div class="carousel-item">
-                    <img class="d-block w-100" src="{{$eventHigh[$i]->img}}" height="300vw"  alt="{{$eventHigh[$i]->title}} slide">
+                    <div class="d-flex justify-content-center">
+                    <img class="d-block" src="{{$eventHigh[$i]->img}}" height="300vw"  alt="{{$eventHigh[$i]->title}} slide">
+                    </div>
                 </div>
            {{--  @endif --}}
         @endfor
@@ -24,12 +28,12 @@
     </div>
 
     <!--Carousel controls--> 
-    <a class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev"> 
+    <a class="carousel-control-prev" type="button" data-bs-target="#myCarousel"  data-bs-slide="prev"> 
         <span class="carousel-control-prev-icon" aria-hidden="true"></span> 
-        <!-- <span class="visually hidden">Previous</span> -->
+       
     </a> 
     <a class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next"> 
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <!-- <span class="visually hidden">Next</span> -->
+        
     </a>
 </div>
